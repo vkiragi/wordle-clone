727941b41f832ded84a53c5f7b8547ac219efdcc
vkiragi <varunkiragi@icloud.com>
2024-09-05 19:37:20 -0700

    keyboard made

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
 function Keyboard() {
+  const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+  const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+
   return (
     <div className="keyboard">
-      <div className="line1"></div>
+      <div className="line1">
+        {keys1.map((key) => {
+          return <div>{key}</div>;
+        })}
+      </div>
       <div className="line2"></div>
       <div className="line3"></div>
     </div>
